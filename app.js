a645605d4e6b41353aa0e32a36c9af6ab368010f
dok <jorgecbll@gmail.com>
2016-05-27 15:54:55 -0400

    it looks better now.

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
+const vm = new Vue({
+  el: '#app',
 
-    const vm = new Vue({
-      el: '#app',
-
+  data: {
+    articles: [],
+    searchQuery: ''
+  },
+  methods: {
+    search: function() {
+      this.$http({
+        url: 'https://en.wikipedia.org/w/api.php',
+        method: 'JSONP',
         data: {
-        articles: [],
-        searchQuery: ''
-      },
-      methods: {
-        search: function () {
-          this.$http({
-          url:'https://en.wikipedia.org/w/api.php',
-          method: 'JSONP',
-          data: {
-            format: 'json',
-            action: 'query',
-            generator: 'search',
-            gsrnamespace: 0,
-            prop: 'extracts',
+          format: 'json',
+          action: 'query',
+          generator: 'search',
+          gsrnamespace: 0,
+          prop: 'extracts',
 
-            exintro: true,
-            exsentences: 3,
-            exlimit: 'max',
-            gsrsearch: this.searchQuery,
-            callback: 'JSON_CALLBACK'
-            }
-        }).then(this.setData.bind(this));
-      },
-      setData: function(res) {
-        this.articles = res.data.query.pages;
-      },
+          exintro: true,
+          exsentences: 3,
+          exlimit: 'max',
+          gsrsearch: this.searchQuery,
+          callback: 'JSON_CALLBACK'
+        }
+      }).then(this.setData.bind(this));
     },
-  })
-    Vue.transition('fade', {
-      enterClass: 'fadeIn',
-      leaveClass: 'fadeOutRight'
-    })
+    setData: function(res) {
+      this.articles = res.data.query.pages;
+    },
+  },
+})
+Vue.transition('fade', {
+  enterClass: 'fadeIn',
+  leaveClass: 'fadeOutRight'
+})
